d2da193eb0df30608ea51b1c0626376ae3d2d7b9
B-smd <djaja@iinet.net.au>
2022-06-16 18:22:54 +0800

    c+p

---
+++ README.md
@@ -0,0 +1,31 @@
+# Weather-Dashboard
+
+## Description
+This weather dashboard allows users to search for a city to get the current weather and 5 day forecast. It will display the city name, the date, an icon reprentation of weather conditions, the temperature, the humidity, the wind speed and the UV index. The UV index will be color code. This Weather Dasboard was created using The Open Weather Map data API, allow me to access their data and functionality by making request with a specific parameters to a URL with a subscription to get an APIkey, in order to get all the information about the weather condition. The initial search based on city name returns latitute and longitude coordinates, and these coordinates are used in the UV index get request.Also I used Javascript Date object, in order to display dates on the current condition and forecast, return by the Open Weather Map response is converted into a Date, then getDate, getMonth and getFullYear methods to generate the date strings displayed on the page. I used One Call API for free subscrition. To get Quickly design and customize responsive mobile-first site using Bootstrap is very helpfull as a powerful JavaScrip plugins. Bootstrap includes HTML and CSS based design templates for typography, forms, buttons, tables, navigation, modals, image carousels and many others.
+
+## Demo
+![Weather-Dashboard](./img/ScreenShot.png)
+![Weather-Dashboard](./img/ScreenShot1.png)
+
+## Link to Website
+
+https://b-smd.github.io/Weather-Dashboard/
+
+https://github.com/B-smd/Weather-Dashboard
+
+## Technologies Used
+- HTML
+- CSS
+- JavaSript
+- Bootstrap
+- Third-party APIs
+
+## Usage
+As a user you can type the city you want to know the weather condition in the form inputs, then you will presented with current and future conditions for that city, this city will be added to search history. You can view current weather conditions for that city, presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index with a color that indicates weather the conditions are favorable, moderate, or severe. You can view future weather conditions for that city and it is presented with 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed. If you click one of a city in the search history, the current and future weather conditions will appear for that city.
+
+## Contributing
+Pull request are welcome, you can make a constribution at the bottom of any docs page to make small changes such as a typo, sentence fix or a broken link. For major changes, please open an issue first to discuss what you would like to change.
+
+## Licence
+[MIT]
+(http)://choosealicence.com/licences/mit/)
---
+++ assets/js/script.js
@@ -0,0 +1,219 @@
+const cityFormEl=document.querySelector("#city-search-form");
+const cityInputEl=document.querySelector("#city");
+const weatherContainerEl=document.querySelector("#current-weather-container");
+const citySearchInputEl = document.querySelector("#searched-city");
+const forecastTitle = document.querySelector("#forecast");
+const forecastContainerEl = document.querySelector("#fiveday-container");
+const pastSearchButtonEl = document.querySelector("#past-search-buttons");
+const cities = [];
+
+const formSumbitHandler = function(event){
+    event.preventDefault();
+    const city = cityInputEl.value.trim();
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({city});
+        cityInputEl.value = "";
+    } else{
+        alert("Please enter a City");
+    }
+    saveSearch();
+    pastSearch(city);
+}
+
+const saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
+
+const getCityWeather = function(city){
+    const apiKey ="f388b8996c977f765ac2b5ad8af811b2"
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+            displayWeather(data, city);
+        });
+    });
+};
+
+
+function kelvinToCelcius(kelvin){
+    return kelvin - 273.15;
+}
+
+const displayWeather = function(weather, searchCity){
+   //clear old content
+   weatherContainerEl.textContent= "";  
+   citySearchInputEl.textContent=searchCity;
+
+
+   //create date element
+   const currentDate = document.createElement("span")
+   currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+   citySearchInputEl.appendChild(currentDate);
+
+   //create an image element
+   const weatherIcon = document.createElement("img")
+   weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+   citySearchInputEl.appendChild(weatherIcon);
+
+   //create a span element to hold temperature data
+   const temperatureEl = document.createElement("span");
+   temperatureEl.textContent = "Temperature: " + kelvinToCelcius(weather.main.temp).toFixed(2) + " °C";
+   temperatureEl.classList = "list-group-item";
+  
+   //create a span element to hold Humidity data
+   const humidityEl = document.createElement("span");
+   humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+   humidityEl.classList = "list-group-item"
+
+   //create a span element to hold Wind data
+   const windSpeedEl = document.createElement("span");
+   windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+   windSpeedEl.classList = "list-group-item"
+
+   //append to container
+   weatherContainerEl.appendChild(temperatureEl);
+
+   //append to container
+   weatherContainerEl.appendChild(humidityEl);
+
+   //append to container
+   weatherContainerEl.appendChild(windSpeedEl);
+
+   const lat = weather.coord.lat;
+   const lon = weather.coord.lon;
+   getUvIndex(lat,lon)
+}
+
+const getUvIndex = function(lat,lon){
+    const apiKey ="f388b8996c977f765ac2b5ad8af811b2"
+    const apiURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+            displayUvIndex(data)
+        });
+    });
+}
+
+ 
+const displayUvIndex = function(index){
+    const uvIndexEl = document.createElement("div");
+    uvIndexEl.textContent = "UV Index: "
+    uvIndexEl.classList = "list-group-item"
+
+    uvIndexValue = document.createElement("span")
+    uvIndexValue.textContent = index.value
+
+    if(index.value <=2){
+        uvIndexValue.classList = "favorable"
+    }else if(index.value >2 && index.value<=8){
+        uvIndexValue.classList = "moderate "
+    }
+    else if(index.value >8){
+        uvIndexValue.classList = "severe"
+    };
+
+    uvIndexEl.appendChild(uvIndexValue);
+
+    //append index to current weather
+    weatherContainerEl.appendChild(uvIndexEl);
+}
+
+const get5Day = function(city){
+    const apiKey ="f388b8996c977f765ac2b5ad8af811b2"
+    const apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+           display5Day(data);
+        });
+    });
+};
+
+const display5Day = function(weather){
+    forecastContainerEl.textContent = ""
+    forecastTitle.textContent = "5-Day Forecast:";
+
+    const forecast = weather.list;
+        for (let i=5; i < forecast.length; i=i+8){
+       const dailyForecast = forecast[i];
+        
+       
+       const forecastEl=document.createElement("div");
+       forecastEl.classList = "card bg-primary text-light m-2";
+
+
+       //create date element
+       const forecastDate = document.createElement("h5")
+       forecastDate.textContent= moment.unix(dailyForecast.dt).format("MMM D, YYYY");
+       forecastDate.classList = "card-header text-center"
+       forecastEl.appendChild(forecastDate);
+
+       
+       //create an image element
+       const weatherIcon = document.createElement("img")
+       weatherIcon.classList = "card-body text-center";
+       weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png`);  
+
+       //append to forecast card
+       forecastEl.appendChild(weatherIcon);
+       
+       //create temperature span
+       const forecastTempEl=document.createElement("span");
+       forecastTempEl.classList = "card-title text-center";
+       forecastTempEl.textContent = "Temp: " + kelvinToCelcius(dailyForecast.main.temp).toFixed(2) + " °C";
+
+        //append to forecast card
+        forecastEl.appendChild(forecastTempEl);
+        
+        const forecastWindSEl=document.createElement("span");
+        forecastWindSEl.classList = "card-title text-center";
+        forecastWindSEl.textContent =  "Wind Speed: " + dailyForecast.wind.speed + "  MPH";
+ 
+        //append to forecast card
+        forecastEl.appendChild(forecastWindSEl);
+
+       const forecastHumEl=document.createElement("span");
+       forecastHumEl.classList = "card-title text-center";
+       forecastHumEl.textContent = "Humidity: " + dailyForecast.main.humidity + "  %";
+
+       //append to forecast card
+       forecastEl.appendChild(forecastHumEl);
+
+
+        //append to five day container
+        forecastContainerEl.appendChild(forecastEl);
+    }
+
+}
+
+const pastSearch = function(pastSearch){
+ 
+
+    pastSearchEl = document.createElement("button");
+    pastSearchEl.textContent = pastSearch;
+    pastSearchEl.classList = "d-flex w-100 btn-light border p-2";
+    pastSearchEl.setAttribute("data-city",pastSearch)
+    pastSearchEl.setAttribute("type", "submit");
+
+    pastSearchButtonEl.prepend(pastSearchEl);
+}
+
+
+const pastSearchHandler = function(event){
+    const city = event.target.getAttribute("data-city")
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+    }
+}
+
+// pastSearch();
+
+cityFormEl.addEventListener("submit", formSumbitHandler);
+pastSearchButtonEl.addEventListener("click", pastSearchHandler);
